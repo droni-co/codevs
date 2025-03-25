87c45295003e4d5d174778952abe570a22c26dc4
Gustavo Barragan <dev@droni.co>
2025-03-25 13:16:32 -0500

    room navigator

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1>Home Page</h1>
-  </div>
+  <UContainer class="py-4">
+    <UCard>
+      <template #header>
+        <div class="flex justify-between items-center">
+          <h2 class="text-lg font-semibold">Rooms</h2>
+          <UButton to="/" icon="i-meteor-icons:plus">
+            Create Room
+          </UButton>
+        </div>
+      </template>
+      <div v-for="room in rooms" :key="room.id">
+        <NuxtLink :to="`/rooms/${room.code}`">
+          <h3 class="text-lg font-semibold">{{ room.name }}</h3>
+        </NuxtLink>
+        <p>{{ room.description }}</p>
+      </div>
+    </UCard>
+  </UContainer>
 </template>
+<script setup lang="ts">
+const rooms = (await useFetch<Room[]>('/api/rooms')).data;
+</script>
---
+++ pages/rooms/[code].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Room: {{ route.params.code }}</h1>
+    {{ room }}
+  </div>
+</template>
+<script setup lang="ts">
+const route = useRoute();
+
+const room = (await useFetch<Room>(`/api/rooms/${route.params.code}`)).data;
+</script>
