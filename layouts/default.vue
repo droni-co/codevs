8ae70c051f6a11403a782dea04dad2a9bd5df356
Gustavo Barragan <dev@droni.co>
2025-03-25 11:13:44 -0500

    feat: ui colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,50 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
-  </div>
+  <UApp locale="es" class="h-screen">
+    <button
+      data-drawer-target="default-sidebar"
+      data-drawer-toggle="default-sidebar"
+      aria-controls="default-sidebar"
+      type="button"
+      class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+    >
+      <span class="sr-only">Open sidebar</span>
+      <UIcon name="i-meteor-icons:bars" class="size-5" />
+    </button>
+
+    <aside
+      id="default-sidebar"
+      class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+      aria-label="Sidebar"
+    >
+      hola mundo
+      <ClientOnly v-if="!colorMode?.forced">
+        <UButton
+          :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
+          color="neutral"
+          variant="ghost"
+          @click="isDark = !isDark"
+        />
+
+        <template #fallback>
+          <div class="size-8" />
+        </template>
+      </ClientOnly>
+    </aside>
+    <div class="p-4 sm:ml-64">
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </div>
+  </UApp>
 </template>
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+</script>
